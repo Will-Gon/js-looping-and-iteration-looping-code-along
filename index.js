6c78a9fa5d4cf93d7a67ed8edd2f7ddd6e5ccd3a
Will-Gon <will.colocho@gmail.com>
2020-11-17 22:01:42 -0800

    commit tree ae0e342780b913757d64a3ccd572aeecad80d185

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    const names = []
+    for (let i = 0; i < array.length; i++) {
+    names.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return names
+}
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n--)
+    }
+}
